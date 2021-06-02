571435d0845c3ce2b9b96157ee3c136f137ddfb5
Chad Hinds <chadrickj8@gmail.com>
2021-06-02 14:10:08 -0400

    Use reflect to get the meta data reltated to the plane object

--- metaData.ts
+++ metaData.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 const plane = { color: 'red' }
 
 Reflect.defineMetadata('note', 'hi', plane);
+Reflect.defineMetadata('height', 11, plane);
 
-const note = Reflect.getMetadata("note", plane)
-console.log(note)
+const note = Reflect.getMetadata("note", plane, 'color')
+
