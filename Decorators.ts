4c577b38436c7e72e266af023d5ba1758526f518
Chad Hinds <chadrickj8@gmail.com>
2021-06-02 14:26:23 -0400

    Add a decorator that operates on the entire class

--- Decorators.ts
+++ Decorators.ts
@@ -32,7 +32,7 @@ function parameterDecorator(target: any, key: string, index: number) {
 function testDecorator(target: any, key: string) {
     console.log(key);
 }
-// Stuff about errors.
+// Stuff about errors
 function logError(errorMessage: string) {
     return function (target: any, key: string, desc: PropertyDescriptor): void {
         const method = desc.value;
