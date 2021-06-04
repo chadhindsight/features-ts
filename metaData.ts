59fa6b190e91414abfa91af73a1f656d03e6cfbf
Chad Hinds <chadrickj8@gmail.com>
2021-06-04 10:09:48 -0400

    Update meta data file with a more recent example

--- metaData.ts
+++ metaData.ts
@@ -1,9 +1,25 @@
 import 'reflect-metadata';
 
-const plane = { color: 'red' }
+@controller
+class Plane {
+    color: string = 'red';
 
-Reflect.defineMetadata('note', 'hi', plane);
-Reflect.defineMetadata('height', 11, plane);
+    @get('/login')
+    fly(): void {
+        console.log('vrrrrrrr');
+    }
+}
 
-const note = Reflect.getMetadata("note", plane, 'color')
+function get(path: string) {
+    return function (target: Plane, key: string) {
+        Reflect.defineMetadata('path', path, target, key);
+    };
+}
 
+function controller(target: typeof Plane) {
+    for (let key in target.prototype) {
+        const path = Reflect.getMetadata('path', target.prototype, key);
+        const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+
+    }
+}
